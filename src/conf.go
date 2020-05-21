a882f81ef6efa4f429f9f5ad1465d843924f3f45
pithing <245853371@qq.com>
2020-05-21 17:36:48 +0800

    init

---
+++ src/conf.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/BurntSushi/toml"
+	"github.com/axgle/mahonia"
+	"runtime"
+)
+
+type Conf struct {
+	Local   string
+	Remote  string
+	Version [8]byte
+	Link    []Link
+}
+type Link struct {
+	Bind   int
+	Remote string
+}
+
+func ConfDefault() Conf {
+	var cf Conf
+	f, _ := os.OpenFile("./pawer.toml", os.O_RDONLY, 0)
+	defer f.Close()
+	b, _ := ioutil.ReadAll(f)
+	if runtime.GOOS == "windows" {
+		enc := mahonia.NewDecoder("gbk")
+		_, data, _ := enc.Translate(b, true)
+		fmt.Println(string(data))
+		_, err := toml.Decode(string(data), &cf)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		_, err := toml.Decode(string(b), &cf)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return cf
+}
--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,8 @@
 module pawer
 
 go 1.14
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394
+)
